7252b13e023c936af5841ee02defe46def87cd25
taewan0530 <kimtaewan@kimtaewan-ui-Mac-mini.local>
2016-01-06 10:25:35 +0900

    bugfix

--- TWImageButton.swift
+++ TWImageButton.swift
@@ -46,28 +46,28 @@ public class TWImageButton: UIButton {
     
     @IBInspectable var beforeSpacing: CGFloat = 0
     @IBInspectable var afterSpacing: CGFloat = 0
+    
 }
 
 // MARK - layout
 extension TWImageButton {
-    private func imageLayout(imageView: UIImageView, image: UIImage){
-        imageView.image = image
-        imageView.bounds.size = image.size
-    }
+    
+    
     
     override public func layoutSubviews() {
         super.layoutSubviews()
         
         let contentRect = contentFrame()
         
-        let tx: CGFloat = vertical ? 0 : (beforeImageRect.width - afterImageRect.width)/2 + (beforeSpacing - afterSpacing)/2
-        let ty: CGFloat =  vertical ? (beforeImageRect.height - afterImageRect.height)/2 + (beforeSpacing - afterSpacing)/2 : 0
-        contentOffset(x: tx, y: ty)
+        if vertical {
+            self.contentEdgeInsets = UIEdgeInsetsMake(beforeImageRect.size.height - afterImageRect.size.height + (beforeSpacing - afterSpacing), 0, 0, 0)
+        } else {
+            self.contentEdgeInsets = UIEdgeInsetsMake(0, beforeImageRect.size.width - afterImageRect.size.width + (beforeSpacing - afterSpacing), 0, 0)
+        }
         
         if let beforeImageView = _beforeImageView {
             var point = contentRect.origin
-            point.x += tx
-            point.y += ty
+            
             if vertical {
                 point.x += (contentRect.size.width - beforeImageRect.size.width)/2
                 point.y -= beforeImageRect.size.height + beforeSpacing
@@ -80,8 +80,6 @@ extension TWImageButton {
         
         if let afterImageView = _afterImageView {
             var point = contentRect.origin
-            point.x += tx
-            point.y += ty
             if vertical {
                 point.x += (contentRect.size.width - afterImageRect.size.width)/2
                 point.y += contentRect.size.height + afterSpacing
@@ -111,8 +109,12 @@ extension TWImageButton {
             size.width += beforeImageRect.width + afterImageRect.width + beforeSpacing + afterSpacing
             size.height = max(size.height, max(beforeImageRect.height, afterImageRect.height))
         }
-        
         return size
+    }
+    
+    private func imageLayout(imageView: UIImageView, image: UIImage){
+        imageView.image = image
+        imageView.bounds.size = image.size
     }
     
     private func contentFrame() -> CGRect {
@@ -123,19 +125,12 @@ extension TWImageButton {
         let w = imageViewRect.width + titleLabelRect.width
         let h = max(imageViewRect.height, titleLabelRect.height)
         
-        let x = centerPoint.x - (imageViewRect.width + titleLabelRect.width)/2
-        let y = centerPoint.y - h/2
+        var x = centerPoint.x - (imageViewRect.width + titleLabelRect.width)/2
+        var y = centerPoint.y - h/2
+        
+        x += vertical ? 0 : (beforeImageRect.width - afterImageRect.width)/2 + (beforeSpacing - afterSpacing)/2
+        y += vertical ? (beforeImageRect.height - afterImageRect.height)/2 + (beforeSpacing - afterSpacing)/2 : 0
         
         return CGRectMake(x, y, w, h)
     }
-    
-    private func contentOffset(x x: CGFloat, y: CGFloat){
-        if let imageView = self.imageView {
-            imageView.transform = CGAffineTransformMakeTranslation(x, y)
-        }
-        if let titleLabel = self.titleLabel {
-            titleLabel.transform = CGAffineTransformMakeTranslation(x, y)
-        }
-    }
-    
 }
